0c29b8ebc3063458cf99ddb2a35afe2a32ba8aa5
Simon Chabot <simon.chabot@logilab.fr>
2021-04-15 12:13:54 +0200

    feat(r_codes): add the huseby function
    the huseby function provided by this commit is the “simple” version, ie without
    oversampling.
    
    related: #10

---
+++ resourcecode/huseby.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from itertools import product
+
+import numpy as np
+
+
+def huseby(X, prob, ntheta, npt, standard="biv", method=3, FORM=0):
+    # cas sans oversampling
+    N, M = X.shape
+
+    assert X.shape[1] == 3, "not handled"
+    assert X.shape[1] == len(prob), "not handled"
+    assert ntheta == npt, "oversampling not handled"
+    assert standard == "biv", "not handled"
+    assert method == 3, "not handled"
+    assert FORM == 0, "not handled"
+
+    # normalisation
+    X_mean = X.mean(axis=0)
+    X_std = X.std(axis=0, ddof=1)  # use ddof=1, to have the same output as R
+    X = (X - X_mean) / X_std
+
+    Q = (X.T @ X) / (N - 1)
+    L = np.linalg.cholesky(Q).T  # use .T as R returned the transposed.
+    X = np.linalg.solve(L.T, X.T).T
+
+    # calculs des quantiles
+    k = N * prob + 0.5
+    kf = np.floor(k).astype(int) - 1
+    kc = np.ceil(k).astype(int) - 1
+    dk = k - kf - 1
+
+    # calculs des angles
+    theta = np.arange(0, ntheta) * 360 / ntheta * np.pi / 180
+    ctheta = np.cos(theta)
+    stheta = np.sin(theta)
+    dtheta = np.diff(theta)
+    sindtheta = np.sin(dtheta)  # noqa
+
+    C = np.ones((ntheta, ntheta, len(prob)), dtype=float)
+    indj = np.hstack(
+        (np.arange(0, ntheta / 4 + 1), ntheta - np.arange(1, ntheta / 4 + 1))
+    ).astype(int)
+    nindj = ntheta / 2 + 1
+    ncdir = int(ntheta * nindj)
+    cdir = np.zeros((ncdir, M))
+    Xres = np.empty((ncdir, M))
+    Yres = np.empty((ncdir, M))
+    Zres = np.empty((ncdir, M))
+
+    for icdir, (i, j) in enumerate(product(range(ntheta), indj)):
+        cdirc = np.array([ctheta[i] * ctheta[j], stheta[j], stheta[i] * ctheta[j]])
+        cdir[icdir, :] = cdirc
+        Y = X @ cdirc
+        Y.sort()
+        C[i, j, :] = (Y[kc] - Y[kf]) * dk + Y[kf]
+
+    ps = cdir @ cdir.T
+    ps[ps < 0] = 0
+
+    a = np.arange(ntheta / 4 + 1, ntheta - ntheta / 4).astype(int)
+    b = np.hstack((np.arange(ntheta / 2, ntheta), np.arange(0, ntheta / 2))).astype(int)
+    c = np.hstack(
+        (np.arange(ntheta / 4 - 1, -1, -1), ntheta - np.arange(1, ntheta / 4))
+    ).astype(int)
+    d = ctheta[:, np.newaxis] @ ctheta[np.newaxis, indj]
+    e = np.repeat(stheta[indj], ntheta).reshape((-1, ntheta)).T
+    f = stheta[:, np.newaxis] @ ctheta[np.newaxis, indj]
+
+    for iprob in range(len(prob)):
+        tmp = C[:, indj, iprob].flatten()
+        dmin = (tmp / ps).min(axis=1).reshape((-1, len(indj))).T
+
+        # XXX how optimize this loop ?
+        for i, index in enumerate(indj):
+            C[:, index, iprob] = dmin[i]
+
+        C[:, a, iprob] = C[:, c, iprob][b]
+
+        tmp = d * C[:, indj, iprob]
+
+        Xres[:, iprob] = tmp.T.flat
+
+        tmp = e * C[:, indj, iprob]
+        Yres[:, iprob] = tmp.T.flat
+
+        tmp = f * C[:, indj, iprob]
+        Zres[:, iprob] = tmp.T.flat
+
+        # Scaling back
+
+        Zres[:, iprob] = (
+            L[0, 2] * X_std[2] * Xres[:, iprob]
+            + L[1, 2] * X_std[2] * Yres[:, iprob]
+            + L[2, 2] * X_std[2] * Zres[:, iprob]
+            + X_mean[2]
+        )
+
+        Yres[:, iprob] = (
+            L[0, 1] * X_std[1] * Xres[:, iprob]
+            + L[1, 1] * X_std[1] * Yres[:, iprob]
+            + X_mean[1]
+        )
+
+        Xres[:, iprob] = L[0, 0] * X_std[0] * Xres[:, iprob] + X_mean[0]
+
+    return Xres, Yres, Zres, theta
