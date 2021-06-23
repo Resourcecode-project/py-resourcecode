c1754a369e1a332222d34bbdcef5d8a3ec3ad977
Simon Chabot <simon.chabot@logilab.fr>
2021-06-23 16:21:56 +0200

    feat(notebooks): add a jupyter notebook to make weather window plots

---
+++ resourcecode/weatherwindow/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from resourcecode.weatherwindow.weatherwindow import (
+    compute_weather_windows,
+    fit_weibull_distribution,
+    WeibullDistributionResult,
+    WeatherWindowResult,
+)
+
+__all__ = [
+    "compute_weather_windows",
+    "fit_weibull_distribution",
+    "WeibullDistributionResult",
+    "WeatherWindowResult",
+]
