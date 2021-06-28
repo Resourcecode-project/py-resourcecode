65ac92bee2a9f5a26d5288c1c37e39fd7b1f7b0d
Simon Chabot <simon.chabot@logilab.fr>
2021-06-28 14:41:13 +0200

    feat(huseby): implement the 2D mode
    closes: #13

--- resourcecode/nataf/huseby.py
+++ resourcecode/nataf/huseby.py
@@ -12,29 +12,36 @@ def huseby(X: np.ndarray, prob: np.ndarray, ntheta: int):
     Parameters
     ----------
 
-    X: a numpy array of size [NxM].
+    X: a numpy array of size [NxM], with M = 2 or 3.
        This should be the output of the nataf simulation.
-    prob: a numpy array of size [1xN] describing the probability level of the
-          contours
+    prob: a numpy array of size [1xM], with M = 2 or 3, describing the
+          probability level of the contours
     ntheta: the number of angles on [0, 360[, used for the calculation.
             it must be a multiple of 4.
 
     Returns
     -------
 
-    (X, Y, Z, theta)
+    if M == 2
+        (X, Y, theta)
 
-    X: a numpy array of size [DxM]
-    Y: a numpy array of size [DxM]
-    Z: a numpy array of size [DxM]
-    theta: a numpy aray of size [DxM]
+        X: a numpy array of size [DxM]
+        Y: a numpy array of size [DxM]
+        theta: a numpy array of size [DxM]
+
+    if M == 3
+        (X, Y, Z, theta)
+
+        X: a numpy array of size [DxM]
+        Y: a numpy array of size [DxM]
+        Z: a numpy array of size [DxM]
+        theta: a numpy array of size [DxM]
     """
 
-    # cas sans oversampling
     N, M = X.shape
 
-    assert X.shape[1] == 3, "not handled"
-    assert X.shape[1] == len(prob), "not handled"
+    assert X.shape[1] in (2, 3), "Only 2d and 3d cases are handled"
+    assert X.shape[1] == len(prob), "The probability levels must be the same size as X"
     assert ntheta % 4 == 0, "ntheta must be a multiple of 4"
 
     # normalisation
@@ -57,71 +64,99 @@ def huseby(X: np.ndarray, prob: np.ndarray, ntheta: int):
     ctheta = np.cos(theta)
     stheta = np.sin(theta)
 
-    C = np.ones((ntheta, ntheta, len(prob)), dtype=float)
-    indj = np.hstack(
-        (np.arange(0, ntheta / 4 + 1), ntheta - np.arange(1, ntheta / 4 + 1))
-    ).astype(int)
-    nindj = ntheta / 2 + 1
-    ncdir = int(ntheta * nindj)
-    cdir = np.zeros((ncdir, M))
-    Xres = np.empty((ncdir, M))
-    Yres = np.empty((ncdir, M))
-    Zres = np.empty((ncdir, M))
+    if M == 2:
+        C = np.ones((ntheta, len(prob)), dtype=float)
+        for i in range(ntheta):
+            Y = X @ np.array([ctheta[i], stheta[i]])
+            Y.sort()
+            C[i, :] = (Y[kc] - Y[kf]) * dk + Y[kf]
 
-    for icdir, (i, j) in enumerate(product(range(ntheta), indj)):
-        cdirc = np.array([ctheta[i] * ctheta[j], stheta[j], stheta[i] * ctheta[j]])
-        cdir[icdir, :] = cdirc
-        Y = X @ cdirc
-        Y.sort()
-        C[i, j, :] = (Y[kc] - Y[kf]) * dk + Y[kf]
+        ps = (
+            ctheta[:, np.newaxis] @ ctheta[:, np.newaxis].T
+            + stheta[:, np.newaxis] @ stheta[:, np.newaxis].T
+        )
+        ps[ps < 0] = 0
 
-    ps = cdir @ cdir.T
-    ps[ps < 0] = 0
+        for iprob in range(len(prob)):
+            C[:, iprob] = (C[:, iprob] / ps).min(axis=1)
 
-    a = np.arange(ntheta / 4 + 1, ntheta - ntheta / 4).astype(int)
-    b = np.hstack((np.arange(ntheta / 2, ntheta), np.arange(0, ntheta / 2))).astype(int)
-    c = np.hstack(
-        (np.arange(ntheta / 4 - 1, -1, -1), ntheta - np.arange(1, ntheta / 4))
-    ).astype(int)
-    d = ctheta[:, np.newaxis] @ ctheta[np.newaxis, indj]
-    e = np.repeat(stheta[indj], ntheta).reshape((-1, ntheta)).T
-    f = stheta[:, np.newaxis] @ ctheta[np.newaxis, indj]
-
-    for iprob in range(len(prob)):
-        tmp = C[:, indj, iprob].flatten()
-        dmin = (tmp / ps).min(axis=1).reshape((-1, len(indj))).T
-
-        # XXX how optimize this loop ?
-        for i, index in enumerate(indj):
-            C[:, index, iprob] = dmin[i]
-
-        C[:, a, iprob] = C[:, c, iprob][b]
-
-        tmp = d * C[:, indj, iprob]
-
-        Xres[:, iprob] = tmp.T.flat
-
-        tmp = e * C[:, indj, iprob]
-        Yres[:, iprob] = tmp.T.flat
-
-        tmp = f * C[:, indj, iprob]
-        Zres[:, iprob] = tmp.T.flat
+        Xres = ctheta[:, np.newaxis] * C
+        Yres = stheta[:, np.newaxis] * C
 
         # Scaling back
+        Yres = L[0, 1] * X_std[1] * Xres + L[1, 1] * X_std[1] * Yres + X_mean[1]
+        Xres = L[0, 0] * X_std[0] * Xres + X_mean[0]
 
-        Zres[:, iprob] = (
-            L[0, 2] * X_std[2] * Xres[:, iprob]
-            + L[1, 2] * X_std[2] * Yres[:, iprob]
-            + L[2, 2] * X_std[2] * Zres[:, iprob]
-            + X_mean[2]
+        return Xres, Yres, theta
+
+    if M == 3:
+        C = np.ones((ntheta, ntheta, len(prob)), dtype=float)
+        indj = np.hstack(
+            (np.arange(0, ntheta / 4 + 1), ntheta - np.arange(1, ntheta / 4 + 1))
+        ).astype(int)
+        nindj = ntheta / 2 + 1
+        ncdir = int(ntheta * nindj)
+        cdir = np.zeros((ncdir, M))
+        Xres = np.empty((ncdir, M))
+        Yres = np.empty((ncdir, M))
+        Zres = np.empty((ncdir, M))
+
+        for icdir, (i, j) in enumerate(product(range(ntheta), indj)):
+            cdirc = np.array([ctheta[i] * ctheta[j], stheta[j], stheta[i] * ctheta[j]])
+            cdir[icdir, :] = cdirc
+            Y = X @ cdirc
+            Y.sort()
+            C[i, j, :] = (Y[kc] - Y[kf]) * dk + Y[kf]
+
+        ps = cdir @ cdir.T
+        ps[ps < 0] = 0
+
+        a = np.arange(ntheta / 4 + 1, ntheta - ntheta / 4).astype(int)
+        b = np.hstack((np.arange(ntheta / 2, ntheta), np.arange(0, ntheta / 2))).astype(
+            int
         )
+        c = np.hstack(
+            (np.arange(ntheta / 4 - 1, -1, -1), ntheta - np.arange(1, ntheta / 4))
+        ).astype(int)
+        d = ctheta[:, np.newaxis] @ ctheta[np.newaxis, indj]
+        e = np.repeat(stheta[indj], ntheta).reshape((-1, ntheta)).T
+        f = stheta[:, np.newaxis] @ ctheta[np.newaxis, indj]
 
-        Yres[:, iprob] = (
-            L[0, 1] * X_std[1] * Xres[:, iprob]
-            + L[1, 1] * X_std[1] * Yres[:, iprob]
-            + X_mean[1]
-        )
+        for iprob in range(len(prob)):
+            tmp = C[:, indj, iprob].flatten()
+            dmin = (tmp / ps).min(axis=1).reshape((-1, len(indj))).T
 
-        Xres[:, iprob] = L[0, 0] * X_std[0] * Xres[:, iprob] + X_mean[0]
+            # XXX how optimize this loop ?
+            for i, index in enumerate(indj):
+                C[:, index, iprob] = dmin[i]
 
-    return Xres, Yres, Zres, theta
+            C[:, a, iprob] = C[:, c, iprob][b]
+
+            tmp = d * C[:, indj, iprob]
+
+            Xres[:, iprob] = tmp.T.flat
+
+            tmp = e * C[:, indj, iprob]
+            Yres[:, iprob] = tmp.T.flat
+
+            tmp = f * C[:, indj, iprob]
+            Zres[:, iprob] = tmp.T.flat
+
+            # Scaling back
+
+            Zres[:, iprob] = (
+                L[0, 2] * X_std[2] * Xres[:, iprob]
+                + L[1, 2] * X_std[2] * Yres[:, iprob]
+                + L[2, 2] * X_std[2] * Zres[:, iprob]
+                + X_mean[2]
+            )
+
+            Yres[:, iprob] = (
+                L[0, 1] * X_std[1] * Xres[:, iprob]
+                + L[1, 1] * X_std[1] * Yres[:, iprob]
+                + X_mean[1]
+            )
+
+            Xres[:, iprob] = L[0, 0] * X_std[0] * Xres[:, iprob] + X_mean[0]
+
+        return Xres, Yres, Zres, theta
